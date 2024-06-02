95fde737a4f083f9995a336860fdb49c7a35b2b7
anhphan237 <anh713988@gmail.com>
2024-06-02 20:36:26 +0700

    Q.Anh - Created Admin UI

---
+++ HotelWpfpApp/Admin.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelWpfpApp
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Page
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void ManageCustomerButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Chuyển đến trang Admin khi nhấn nút Login
+            NavigationService?.Navigate(new Customer());
+        }
+
+        private void ManageBookingButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Chuyển đến trang Admin khi nhấn nút Login
+            NavigationService?.Navigate(new Booking());
+        }
+
+        private void ManageRoomButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Chuyển đến trang Admin khi nhấn nút Login
+            NavigationService?.Navigate(new Room());
+        }
+    }
+}
--- HotelWpfpApp/App.xaml.cs
+++ HotelWpfpApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Navigation;
 
 namespace HotelWpfpApp
 {
@@ -9,6 +10,13 @@ namespace HotelWpfpApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            NavigationWindow navigationWindow = new NavigationWindow();
+            navigationWindow.Content = new Login(); // Trang chính của ứng dụng
+            navigationWindow.Show();
+        }
     }
 
 }
